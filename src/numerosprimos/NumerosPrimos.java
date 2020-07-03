a22dbc877783f9f2c7557bb0d7b5e93fd43dc6ca
ivan0474 <ivanordonez0474@gmail.com>
2020-07-02 21:13:18 -0400

    Tarea parte dos

--- src/numerosprimos/NumerosPrimos.java
+++ src/numerosprimos/NumerosPrimos.java
@@ -5,6 +5,8 @@
  */
 package numerosprimos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ivan
@@ -15,7 +17,34 @@ public class NumerosPrimos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+          Scanner obtenerNumero = new Scanner(System.in);
+          System.out.println("Bienvenidos, hoy buscaremos numeros primos y multiplos de `3´");
+        int contador,numero; 
+        System.out.print("Ingresa un numero: ");
+        numero = obtenerNumero.nextInt(); 
+        contador = 0;
+        int i; 
+        for(i = 1; i <= numero; i++)
+        {
+            if((numero % i) == 0)
+            {
+                contador++;
+            }
+        } 
+        if(contador <= 2)
+        {
+            System.out.println("Este numero es primo");
+        }else{
+            System.out.println("Este numero no es primo");
+        }
+        if (numero%3 == 0){
+            System.out.println("Y Si es multiplo de 3");
+        }else 
+        {System.out.println("Y No es multiplo de 3");
+        }
+        System.out.println("Gracias por utilizar nuestros servicios");    
+        }
     }
+
     
-}
+    
